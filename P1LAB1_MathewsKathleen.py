bf4b6d5cc20c0d9f17b66afb34c83245e2d865d0
mathewsk4737 <160441462+mathewsk4737@users.noreply.github.com>
2024-02-18 19:40:04 -0500

    Add files via upload
    First Submission.

---
+++ P1LAB1_MathewsKathleen.py
@@ -0,0 +1,9 @@
+#Kathleen Mathews
+#2/18/23
+#P1Lab1
+#In this project the user will input their name and then will be greeted with a welcome message.
+
+var_name = input('')
+print('Hey',var_name)
+print('Welcome to zyBooks!')
+
